1eaa45257998735b1a4fe4265c8a0c2fa785a821
Robiul8726r <sakilpk6438@gmail.com>
2024-10-25 17:51:45 +0600

    Update reply.js

--- scripts/commands/reply.js
+++ scripts/commands/reply.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "ripley",
-  version: "2.0.1",
+  name: "reply",
+  version: "1.1.0",
   permission: 0,
-  credits: "Robiul",
+  credits: "Farhan",
   description: "noprefix",
   prefix: false,
   category: "user",
@@ -14,22 +14,24 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("love")
-react.includes("jan") || 
-react.includes("sona") ||
-react.includes("love") ||
-react.includes("babu") ||
-react.includes("jano") || 
-react.includes("kiss") || 
-react.includes("xuna") ||
- {
+	if(react.includes("i love you") ||
+     react.includes("love") || 
+react.includes("jan") || react.includes("xan") || react.includes("xanu") || react.includes("sona") ||
+react.includes("xn") ||
+react.includes("xona") || react.includes("xuna") || react.includes("ভালবাসি") ||
+react.includes("ব্রেকাপ") ||
+react.includes("break up") ||
+react.includes("sex") ||
+react.includes("nude") ||  
+react.includes("Fucking") ||
+react.includes("🤤")) {
 		var msg = {
-				body: "- প্রেম ভালোবাসা বলতে কিছু নেই এইসব প্রেম না খাওয়ার ধান্দা-!!😼"
+				body: "- ভালোবাসা বলতে কিছু নেই এইসব প্রেম না খাওয়ার ধান্দা!!😼"
 			}
-			api.sendMessage(msg, threadID, messageID);
+api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😾", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+  }	
